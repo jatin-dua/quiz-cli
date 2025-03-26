fd18bb0b8a40fc07a9e36ae34d5871cf1fc5438f
Jatin Dua <jdua004@gmail.com>
2025-03-26 19:14:27 +0530

    feat: Add random arithmetic problems

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"bufio"
-	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"strconv"
 	"time"
 )
+
+type Problem struct {
+	question string
+	answer   int
+}
 
 func readUserInput(answerCh chan string) {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -15,6 +21,32 @@ func readUserInput(answerCh chan string) {
 		answerCh <- scanner.Text()
 	}
 	close(answerCh)
+}
+
+func generateProblem(problemId, maxn int) Problem {
+	rand.Seed(time.Now().UnixNano())
+	num1 := rand.Intn(maxn) + 1
+	num2 := rand.Intn(maxn) + 1
+
+	operators := []string{"+", "-", "*", "/"}
+	op := operators[rand.Intn(len(operators))]
+
+	var result int
+	question := ""
+
+	switch op {
+	case "+":
+		result = num1 + num2
+	case "-":
+		result = num1 - num2
+	case "*":
+		result = num1 * num2
+	case "/":
+		result = num1 / num2
+	}
+
+	question = fmt.Sprintf("Problem #%d: %d %s %d = ?", problemId, num1, op, num2)
+	return Problem{question: question, answer: result}
 }
 
 func askQuestion(question string, timeLimit time.Duration, answerCh chan string) (string, bool) {
@@ -38,27 +70,13 @@ func askQuestion(question string, timeLimit time.Duration, answerCh chan string)
 }
 
 func main() {
-	var filename string
 	var limit int
+	var maxn int
 
-	flag.StringVar(&filename, "csv", "problems.csv", "a csv file in the format of 'question, answer' (default 'problems.csv')")
+	flag.IntVar(&maxn, "maxn", 100, "the maximum number in a problem (default 100)")
 	flag.IntVar(&limit, "limit", 30, "the time limit for quiz in seconds (default 30)")
 	flag.Parse()
 
-	file, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	reader.FieldsPerRecord = 2
-	data, err := reader.ReadAll()
-	if err != nil {
-		panic(err)
-	}
-
-	N := len(data)
 	correctCnt := 0
 
 	timeLimit := time.Duration(limit) * time.Second
@@ -66,19 +84,33 @@ func main() {
 
 	go readUserInput(answerCh)
 
-	for i, row := range data {
-		ques := fmt.Sprintf("Problem #%d: %s\n", i+1, row[0])
-		ans := row[1]
+	i := 1
+	for {
+		problem := generateProblem(i, maxn)
+		input, answered := askQuestion(problem.question, timeLimit, answerCh)
 
-		input, answered := askQuestion(ques, timeLimit, answerCh)
+		if input == "" && answered {
+			break
+		}
 
-		if ans == input {
+		if !answered {
+			continue
+		}
+
+		userInput, err := strconv.Atoi(input)
+		if err != nil {
+			panic(err)
+		}
+
+		if problem.answer == userInput {
 			fmt.Println("Correct!")
 			correctCnt++
-		} else if answered {
+		} else {
 			fmt.Println("Incorrect!")
 		}
+
+		i++
 	}
 
-	fmt.Printf("You scored %d out of %d.\n", correctCnt, N)
+	fmt.Printf("You scored %d out of %d.\n", correctCnt, i)
 }
